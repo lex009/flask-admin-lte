9c212fe24a66d6334aae5789c0e3de0daa71de92
Alex Belyaev <a.v.belyaev@gmail.com>
2016-03-23 16:58:43 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flask-lte-admin',
+    name='flask-admin-lte',
     packages=find_packages(),
     version='0.2',
     description='Integrates LteAdmin into flask-admin',
