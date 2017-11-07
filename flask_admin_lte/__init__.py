4a0a871ea7e5fdf2191a6a80665266f051ed9e4b
Alex Belyaev <a.v.belyaev@gmail.com>
2017-11-07 20:05:28 +0300

    Revert "fix admin templates"

--- flask_admin_lte/__init__.py
+++ flask_admin_lte/__init__.py
@@ -1,11 +1,10 @@
 __author__ = 'Alex Belyaev'
 __email__ = 'lex@alexbelyaev.com'
 
-from flask import Flask, Blueprint, url_for, request
+from flask import Flask, Blueprint
 from flask.helpers import get_root_path
 import jinja2
 import os
-
 
 def lte(app: Flask):
     for blueprint in app.blueprints.values():
@@ -13,10 +12,4 @@ def lte(app: Flask):
             jinja2.FileSystemLoader(os.path.join(get_root_path('flask_admin_lte'), 'templates/lte')),
             blueprint.jinja_loader,
         ])
-
-    @app.template_filter('if_exists')
-    def if_exists(filename):
-        if os.path.isfile('{}{}'.format(app.root_path, filename)):
-            return filename
-
     app.register_blueprint(Blueprint('admin_lte', __name__, static_folder='static', static_url_path='/static/lte'))
