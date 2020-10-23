fb7a86cecb2d9085c09149cd779dc14ea59a586b
Egor Gusev <guy.do.or.die@gmail.com>
2020-10-23 15:00:57 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
+import atexit
+import sys
+import os
+
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+
+
+class Develop(develop):
+    def run(self):
+        pkn = self.config_vars.get('dist_name')
+
+        def _post_install():
+            def find_module_path():
+                for p in sys.path:
+                    if os.path.isdir(p) and pkn in os.listdir(p):
+                        return os.path.join(p, pkn)
+
+            from subprocess import call
+            call(['yarn'], cwd=find_module_path())
+
+        atexit.register(_post_install)
+        develop.run(self)
+
 
 setup(
     name='flask-admin-lte',
@@ -7,9 +30,7 @@ setup(
     description='Integrates LteAdmin into flask-admin',
     author='Alex Belyaev',
     author_email='a.v.belyaev@gmail.com',
-    setup_requires=[
-        'setuptools-bower'
-    ],
+    cmdclass={'develop': Develop},
     install_requires=[
         'flask-admin>=1.4.0'
     ],
